8d9f9d7450b46d10b9352dbdd10f6de0d47574f4
karthi05ck <karthickece05@gmail.com>
2023-06-28 08:11:46 +0530

    deleted unwanted files

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++
@@ -1,9 +0,0 @@
-package com.qa.opencart.pages;
-
-import org.openqa.selenium.By;
-
-public class OrderPage {
-
-	int a =5;
-	By loc = By.id("test");
-}
--- src/main/java/com/qa/opencart/pages/Sel.java
+++
@@ -1,9 +0,0 @@
-package com.qa.opencart.pages;
-
-import org.openqa.selenium.By;
-
-public class Sel {
-
-	int a =99;
-	By loc = By.id("SEl");
-}
